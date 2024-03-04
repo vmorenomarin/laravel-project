e63b54e07b21f5fbf88405bf09aa9764d469c166
vmorenomarin <vmorenomarin@gmail.com>
2024-03-04 16:06:38 -0500

    Clean reposity directory

--- storage/framework/views/06c3b72525b682d717bbac49ef98c701.php
+++
@@ -1,9 +0,0 @@
-<?php $__env->startSection('content'); ?>
-<h2>Cree un nuevo reporte</h2>
-<form action="">
-    <label for="text">
-        Text
-    </label>
-</form>
-<?php $__env->stopSection(); ?>
-<?php echo $__env->make('templates.principal', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /media/victor/info/victor/Insync/vmorenomarin@gmail.com/Google Drive/git/laravel-project/resources/views/ExpenseReports/create.blade.php ENDPATH**/ ?>
