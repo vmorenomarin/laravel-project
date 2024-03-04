e0a00afc725000073d655183acad287469649e22
vmorenomarin <vmorenomarin@gmail.com>
2024-03-04 16:41:47 -0500

    Fixed HTML in show expense view.

--- public/js/app.js
+++ public/js/app.js
@@ -11,39 +11,42 @@ if (modal !== null) {
         modal.showModal();
     };
 
-    let inputs=document.querySelectorAll('form input')
+    let inputs = document.querySelectorAll("form input");
 
-    inputs.forEach(input=>{
-        input.addEventListener('focus', ()=>{
+    inputs.forEach((input) => {
+        input.addEventListener("focus", () => {
             input.previousElementSibling.classList.add("focus");
-        })
-        input.addEventListener('blur', ()=>{
+        });
+        input.addEventListener("blur", () => {
             input.previousElementSibling.classList.remove("focus");
-        })
-    })
+        });
+    });
 
-    let valueOn=document.querySelector("span.on").dataset.value;
-    let valueOff=document.querySelector("span.off").dataset.value;
-    let iconOn=document.querySelector("span.on").dataset.icon;
-    let iconOff=document.querySelector("span.off").dataset.icon;
-    document.querySelector(".thumb span").textContent=iconOff;
+    let valueOn = document.querySelector("span.on").dataset.value;
+    let valueOff = document.querySelector("span.off").dataset.value;
+    let iconOn = document.querySelector("span.on").dataset.icon;
+    let iconOff = document.querySelector("span.off").dataset.icon;
+    document.querySelector(".thumb span").textContent = iconOff;
     let checkbox = document.getElementById("toggle");
-    if(!document.querySelector("#editMode")) checkbox.value=valueOff;
-    
+    if (!document.querySelector("#editMode")) checkbox.value = valueOff;
+
     function toggle() {
         checkbox.checked = !checkbox.checked;
         checkbox.value = checkbox.checked ? valueOn : valueOff;
         checkbox.icon = checkbox.checked ? iconOn : iconOff;
-        document.querySelector(".thumb span").textContent=checkbox.icon;
+        document.querySelector(".thumb span").textContent = checkbox.icon;
+    }
 
-      }
-      
-      document.querySelector(".switch").addEventListener("click", toggle)
+    document.querySelector(".switch").addEventListener("click", toggle);
 
-      if (document.querySelector("#editMode") && document.querySelector("input#toggle").value == 'Y') toggle();
+    if (
+        document.querySelector("#editMode") &&
+        document.querySelector("input#toggle").value == "S"
+    )
+        toggle();
 
-      let alertDiv = document.querySelector('div.alert')
-      alertDiv.addEventListener('click', ()=>{
-        alertDiv.style.display='none';
-      })
+    let alertDiv = document.querySelector("div.alert");
+    alertDiv.addEventListener("click", () => {
+        alertDiv.style.display = "none";
+    });
 }
